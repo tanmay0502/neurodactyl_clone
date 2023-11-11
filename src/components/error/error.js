1398b8a394014b63d255b8f5cefc393c3bd66b19
tanmay0502 <81376006+tanmay0502@users.noreply.github.com>
2023-11-11 21:38:54 +0530

    Responsive Homepage Completed SDK Product page created

--- src/components/error/error.js
+++ src/components/error/error.js
@@ -5,7 +5,7 @@ function Error() {
   return (
     <div className="error-container">
       <h1>This page is not available currently</h1>
-      <p>Comming soon...</p>
+      <p>Coming soon...</p>
       <Link className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-10' href="/">
         Go to Homepage
       </Link>
